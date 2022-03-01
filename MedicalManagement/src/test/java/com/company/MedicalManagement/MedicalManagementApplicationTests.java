fdea50185499d6abbef4943faf2e8b1e306f47d9
unknown <ilkinmirili@gmail.com>
2022-03-01 21:48:30 +0400

    changes

---
+++ MedicalManagement/src/test/java/com/company/MedicalManagement/MedicalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.MedicalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
